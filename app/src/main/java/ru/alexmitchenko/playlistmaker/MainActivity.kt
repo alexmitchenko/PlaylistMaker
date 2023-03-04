1d72a34ae52f3a0f0d1c68b3cb6aa0546eb45ec1
alexmitchenko <alex.mitchenko@gmail.com>
2023-03-04 21:53:55 +0500

    Обработчики кликов

--- app/src/main/java/ru/alexmitchenko/playlistmaker/MainActivity.kt
+++ app/src/main/java/ru/alexmitchenko/playlistmaker/MainActivity.kt
@@ -3,15 +3,34 @@ package ru.alexmitchenko.playlistmaker
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var btnSearch: Button
+    private lateinit var btnMedia: Button
     private lateinit var btnSettings: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnSearchClickListener: View.OnClickListener = object : View.OnClickListener {
+            override fun onClick(v: View?) {
+                Toast.makeText(this@MainActivity, "Нажата кнопка Поиск", Toast.LENGTH_LONG).show()
+            }
+        }
+        btnSearch = findViewById<Button?>(R.id.btn_search).apply {
+            setOnClickListener(btnSearchClickListener)
+        }
+
+        btnMedia = findViewById<Button?>(R.id.btn_media).apply {
+            setOnClickListener {
+                Toast.makeText(this@MainActivity, "Нажата кнопка Медиатека", Toast.LENGTH_LONG).show()
+            }
+        }
 
         btnSettings = findViewById<Button?>(R.id.btn_settings).apply {
             setOnClickListener {
